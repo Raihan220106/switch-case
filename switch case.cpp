d41cabc0b4bf8743bc0aba3365f61ef8228ee256
Raihan Muhammad Riswandi <165734611+Raihan220106@users.noreply.github.com>
2025-09-03 14:01:32 +0700

    Add files via upload

---
+++ switch case.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main () {
+ //biodata
+ string Nama ;
+ int umur ;
+ string alamat ;
+ int nomertelepon;
+ //input biodata
+ cout << "Masukkan nama :";
+ cin >> Nama;
+ cout << "masukkan umur :";
+ cin >> umur;
+ cout << "masukkan alamat : " ;
+ cin>> alamat;
+ cout << " masukkan nomertelepon : " ;
+ cin nomertelepon;
+
+
+}
+
